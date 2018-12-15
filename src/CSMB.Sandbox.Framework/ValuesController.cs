28ce0a2c280281fc84dd612c94585ce083ca9351
Alex Schouls <alex_schouls@live.com>
2018-12-15 13:27:22 +0100

    Made sandbox API work

---
+++ src/CSMB.Sandbox.Framework/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+
+namespace CSMB.Sandbox.Framework
+{
+    [Route("api/values")]
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult GetAll()
+        {
+            return Json(new int[] { 1, 2, 3, 4, 5 });
+        }
+
+        public IHttpActionResult Get(int[] values)
+        {
+            return Json(values);
+        }
+    }
+}
